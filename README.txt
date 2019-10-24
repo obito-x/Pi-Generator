a1521074eb7187fbb4236d7db89d509a448b8c83
obito-x <53710795+obito-x@users.noreply.github.com>
2019-10-24 14:25:43 +0630

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,45 @@
+
+-About-
+
+
+'This program is the generator of pi with nth decimal place.'
+
+
+advantage  : if you(user) enter other input instead of number , no effect on this program.
+
+diadvantage: program output materials form is depend on the size of your(user's) terminal.
+
+But don't worry. even the view is not clear , this program can work exactly
+ 
+                
+recommend: [ 81 x 24  ]
+use this ratio on your terminal and run this program.
+And view clearly.
+    
+
+    enjoy :)
+
+
+-Obito-
+
+
+
+
+
++++++++++++++++++++++++++++++++++++++
+
+
+Help
+****
+
+ Go to your downloaded folder and connect to internet
+
+For linux user
+
+Open your terminal
+-> apt install python3
+-> pip install requirements
+-> pip install colorama
+-> python3 findpi.py
+
+note: this is example. use your package manager instead of apt
---
+++ findpi.py
@@ -0,0 +1,63 @@
+# find_pi_to_the_nth_digit.py
+# {obito}
+"""
+    -About-
+
+    'This program is the generator of pi with nth decimal place.'
+
+    advantage  : if user enter other input instead of number , no effect on this program.
+    diadvantage: program output materials form is depend on the size of user terminal.
+                 But don't worry. even the view is not clear , this program can work exactly
+                 recommend: [ 81 x 24  ]
+                 use this ratio on your terminal and run this program.
+                 And view clearly.
+    enjoy :)
+"""
+
+
+from colorama import Fore
+
+# variables
+pi = 22/7
+ch = 'y'
+
+
+while ch == 'y':
+    print('\n'*10)  # Space
+
+    print(Fore.RESET + '\n************************************************************\n')
+    print(Fore.LIGHTGREEN_EX + '\n This program is the generator of pi with nth decimal place \n'
+          + Fore.RESET)  # Title
+    print('\n************************************************************\n')
+
+    print('\n'*8)  # Space
+
+    print(Fore.RED + '\nWarning:' + Fore.YELLOW + 'The Maximum: 51!!\n'
+          + ' '*8 + 'Only whole numbers of decimal place can enter!\n' + ' '*8 + 'eg: 0,4,23,51\n'
+          + Fore.RESET)  # Warning
+    # maximum is 51 but allow to enter to 55!!!
+
+    try:
+        n = int(input(Fore.CYAN + "\nEnter the nth decimal place of pi: " + Fore.GREEN))
+    except:
+        continue
+    # user input and error handling
+
+    if n < 56:
+        try:
+            print(Fore.CYAN + '\nPi: ' + Fore.RESET + f'%1.{n}f' % pi)
+        except:
+            continue
+        # if user enter negative values, this error handling can fix
+        # Generate Less than 56
+    if n > 55:
+        print(Fore.CYAN + '\nPi: ' + Fore.RESET + '%1.56f' % pi)
+        print(Fore.LIGHTRED_EX + "\nThe Decimal number after the 51 are zero" + Fore.RESET)
+        # Generate greater than 55
+
+    ch = input(Fore.GREEN + '\nEnter {-y-} to do again: ' + Fore.RESET)  # ask do again
+
+    print('\n'*4)  # Space
+
+
+# ******************************************************************************************** #
